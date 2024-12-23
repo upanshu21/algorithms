b6ebfa59e606b72e935ca7bc4b56b79c0974ef69
upanshu <upanshu21@gmail.com>
2024-12-23 01:16:03 -0500

    unionfind graph algo with java

---
+++ graphs/unionfind/java/UnionFind.java
@@ -0,0 +1,61 @@
+package graphs.unionfind.java;
+
+import java.util.*;
+
+public class UnionFind {
+
+    public List<Integer> parent = new ArrayList<>();
+    public List<Integer> rank = new ArrayList<>();
+
+    public UnionFind(int n) {
+        for (int i = 0; i < n; i++) {
+            parent.add(i);
+            rank.add(1);
+        }
+    }
+
+    public int find(int node) {
+        
+        if (node == parent.get(node)) {
+            return node;
+        }
+
+        parent.set(node, find(parent.get(node)));
+        return parent.get(node);
+    }
+
+    public void union(int node1, int node2) {
+
+        int parent1 = find(node1);
+        int parent2 = find(node2);
+
+        if (parent1 == parent2) {
+            return;
+        }
+
+        if (rank.get(parent1) > rank.get(parent2)) {
+            parent.set(parent2, parent1);
+        } else if (rank.get(parent1) < rank.get(parent2)) {
+            parent.set(parent1, parent2);
+        } else {
+            parent.set(parent2, parent1);
+            rank.set(parent1, rank.get(parent1) + 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        uf.union(1, 2);
+        uf.union(3, 4);
+
+        System.out.println(uf.find(0));
+        System.out.println(uf.find(1));
+        System.out.println(uf.find(2));
+        System.out.println(uf.find(3));
+        System.out.println(uf.find(4));
+
+    }
+}
---
+++ graphs/unionfind/java/UnionFindTest.java
@@ -0,0 +1,74 @@
+package graphs.unionfind.java;
+
+class UnionFindTest {
+
+    public static void main(String[] args) {
+        UnionFindTest test = new UnionFindTest();
+        test.testFindInitiallyReturnsSelf();
+        test.testUnionMergesComponents();
+        test.testUnionHandlesDifferentRanks();
+        test.testFindWithPathCompression();
+        test.testDisconnectedComponents();
+        test.testSelfUnion();
+        System.out.println("All tests passed!");
+    }
+
+    void testFindInitiallyReturnsSelf() {
+        UnionFind uf = new UnionFind(5);
+        for (int i = 0; i < 5; i++) {
+            assert uf.find(i) == i : "Each node should initially be its own parent";
+        }
+    }
+
+    void testUnionMergesComponents() {
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        assert uf.find(0) == uf.find(1) : "Nodes 0 and 1 should have the same root after union";
+
+        uf.union(1, 2);
+        assert uf.find(0) == uf.find(2) : "Nodes 0, 1, and 2 should have the same root after unions";
+    }
+
+    void testUnionHandlesDifferentRanks() {
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1); // Merge 0 and 1
+        uf.union(2, 3); // Merge 2 and 3
+
+        int rankBefore = uf.rank.get(uf.find(0));
+
+        uf.union(0, 2); // Merge the two sets
+
+        assert uf.find(0) == uf.find(3) : "All nodes should have the same root after unions";
+        assert uf.rank.get(uf.find(0)) == rankBefore + 1 : "Rank should increase after merging sets of equal rank";
+    }
+
+    void testFindWithPathCompression() {
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        uf.union(1, 2);
+
+        int root = uf.find(2);
+
+        assert uf.parent.get(0) == root : "Path compression should make all nodes point directly to the root";
+        assert uf.parent.get(1) == root : "Path compression should make all nodes point directly to the root";
+        assert uf.parent.get(2) == root : "Path compression should make all nodes point directly to the root";
+    }
+
+    void testDisconnectedComponents() {
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 1);
+        uf.union(2, 3);
+
+        assert uf.find(0) != uf.find(2) : "Disconnected components should not have the same root";
+        assert uf.find(1) != uf.find(3) : "Disconnected components should not have the same root";
+
+        uf.union(3, 4);
+        assert uf.find(2) == uf.find(4) : "Nodes 2, 3, and 4 should share the same root after unions";
+    }
+
+    void testSelfUnion() {
+        UnionFind uf = new UnionFind(5);
+        uf.union(0, 0);
+        assert uf.find(0) == 0 : "Union of a node with itself should not change its root";
+    }
+}
